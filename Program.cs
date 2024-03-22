afaf1b6f02df702c5a2c9d53fc9983bd778e3a7e
doltishdude <bondarenkojenya321@mail.ru>
2024-03-22 18:19:05 +0300

    domashka 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+namespace homework1
+{
+    internal class Project1Base
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("вы ввели число: ");
+            
+          
+            int x;
+            x = Console.Read();
+
+           
+            Console.WriteLine(x);
+
+            Console.ReadLine();
+        }
+
+        }
+    }
+
+
+
+
