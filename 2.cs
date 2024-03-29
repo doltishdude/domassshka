432600dc983f9cea0a9cc5cf95c39a52a1bb390d
doltishdude <bondarenkojenya321@mail.ru>
2024-03-29 18:06:28 +0300

    update file

---
+++ 2.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+namespace homework_task_3;
+
+internal class ProjectBase
+{
+    static void Main(string[] args)
+    {
+        float x = float.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{x / 100} - число полных метров");
+
+    }
+
+}
+
---
+++ program3.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+namespace aboba
+{
+    internal class Project1Base
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            while (numbers > 0)
+            
+            {
+                numbers--;
+
+                if (numbers % 2 == 0)
+                {
+                    Console.WriteLine(numbers);
+                }
+                if (numbers ==10) { break; }
+
+                   
+
+
+
+                
+
+                
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
