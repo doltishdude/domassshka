b5f93588beeac1986529a1c7a75b5bd23166aeea
doltishdude <bondarenkojenya321@mail.ru>
2024-04-02 19:29:08 +0300

    domashka_02_04_24

---
+++ Program5.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+namespace aboba
+{
+    internal class Project1Base
+    {
+        static void Main(string[] args)
+        {
+            float s = 10;
+            float p = 20.5f;
+            int k = 1;
+
+
+
+            while (s <= 200)
+            {
+                s += (s * p) / 100;
+
+                k++;
+
+
+                if (s > 200)
+                {
+                    Console.WriteLine("суммарный пробег лыжника привысит 200 км через " + k + " дней"); Console.WriteLine( "сууммарный пробег: " + s + "км"); break;
+                }
+
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
