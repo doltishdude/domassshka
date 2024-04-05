592fadaaa6196a5e218271c9ce9abd84ae9f3b89
doltishdude <bondarenkojenya321@mail.ru>
2024-04-05 18:29:33 +0300

    05_04_24 password task

---
+++ Program password.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+namespace password;
+
+internal class Project1Base
+{
+    static void Main(string[] args)
+    {
+        int a = 12345;
+        int popitki;
+
+        for (int i = 5; i > 0; i--)
+        {
+            if (Convert.ToInt32(Console.ReadLine()) == 12345)
+            {
+                Console.WriteLine("зашел");
+                break;
+                
+            }
+            else
+            {
+                Console.WriteLine ("error" );
+                popitki = i;
+            }
+            if (popitki == 1)
+            {
+                Console.WriteLine("попытки закончились");
+            }
+        }
+
+        Console.ReadLine();
+    }
+
+}
+
+
+
+
