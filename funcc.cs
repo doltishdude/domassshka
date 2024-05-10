6b1ebf18584aca9687501d4b93c008e714f089a2
doltishdude <bondarenkojenya321@mail.ru>
2024-05-10 18:35:08 +0300

    classwork_10_05_24

---
+++ funcc.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+using System.IO;
+namespace a;
+
+internal class Project1Base
+{
+    static void Main(string[] args)
+    {
+
+        Func(12);
+        int number2= Func(12);
+
+        Console.WriteLine(number2);
+
+        Console.ReadLine();
+    }
+
+    static int Func (int number)
+    {  return number + 10; }
+        
+}
---
+++ readfile_massive.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+using System.IO;
+namespace aboba
+{
+    internal class Project1Base
+    {
+        static void Main(string[] args)
+        {
+            string[] textfile = File.ReadAllLines("AAA2.txt");
+            char[,] drawing = new char[textfile.Length, textfile.Length];
+            
+            for (int i = 0; i < drawing.GetLength(0); i++) 
+            {
+                for (int j = 0; j < drawing.GetLength(1); j++) 
+
+                { 
+                    drawing[ j, i ] = textfile [ i] [j ];
+                    Console.Write(drawing[ j, i ] );
+                
+                }
+
+                Console.WriteLine();
+            
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
