581d5005746557ff94c6fc64cc16ebc7b081be14
doltishdude <bondarenkojenya321@mail.ru>
2024-05-10 18:46:51 +0300

    tryin to merge branches attempt 3

---
+++ multi-level_massive.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+namespace aboba
+{
+    internal class Project1Base
+    {
+        static void Main(string[] args)
+        {
+            char[,] words = new char[,] {
+
+
+            { 'a', 'b', 'c'},
+            {'d', 'e', 'f'},
+            {'g' , 'k', 'l'}
+           
+            };
+            
+            for ( int i =0 ; i < words.GetLength(0); i++)
+            {
+                for ( int j =0; j < words.GetLength(0); j++ )
+                {
+                    Console.Write(words[i,j] + " ");
+
+                }  
+                Console.WriteLine();
+            }
+
+           Console.ReadLine();
+        }
+    }
+}
+
