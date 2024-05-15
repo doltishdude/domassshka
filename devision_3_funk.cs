88602d8e4e37bc5fef326dbbcde8e3e0ec89d089
doltishdude <bondarenkojenya321@mail.ru>
2024-05-15 11:11:18 +0300

    homework_15_05_24

---
+++ devision_3_funk.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+internal class Project1Base
+{
+    static void Main(string[] args)
+    {
+
+        Func(0);
+        int number2 = Func(0);
+        int arraysum = 0;
+
+        Console.WriteLine(number2);
+
+
+        int[] arraynumbers = { 2, 11, 25, 30, 24, 12, };
+
+
+        for (int i = 0; i < arraynumbers.Length; i++)
+        {
+
+            if (arraynumbers[i] % 3 == 0)
+            {
+
+                arraysum += arraynumbers[i];
+
+            }
+        }
+        Console.WriteLine(arraysum);
+        Console.ReadLine();
+    }
+
+    static int Func(int arraysumm)
+
+    { return arraysumm; }
+
+}
+
---
+++ reversed_stroke-return.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+namespace shkwarki;
+
+internal class Project1Base
+{
+    static void Main(string[] args)
+    {
+
+        Func("эта строка написана задом на перед");
+        string word2 = Func("эта строка написана задом на перед");
+
+        char[] charArray = word2.ToCharArray();
+        Array.Reverse(charArray);
+
+        Console.WriteLine(charArray);
+
+        Console.ReadLine();
+    }
+
+    static string Func(string word)
+    { return word; }
+
+}
+
