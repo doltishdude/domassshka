3a396ea19fc7505e2a570e5ff41d69b3616d930f
doltishdude <bondarenkojenya321@mail.ru>
2024-06-28 16:45:02 +0300

    is-a unfinished task

---
+++ is-a_minecraft.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.Metadata;
+
+internal class Project1Base
+{
+    static void Main(string[] args)
+    {
+        Character[] characters = { new Character(40 + "") };
+        foreach (var character in characters)
+        {
+            character.PrintCharacterInfo();
+        }
+        Enemy creeper = new Enemy(11);
+       
+        Enemy.CreeperDamage();
+        Console.ReadLine();
+    }
+}
+class Character
+{
+    protected int Health;
+    public string Name;
+    protected int Damage;
+    public Character(int Health, string Name, int Damage)
+    {
+        this.Health = Health;
+        this.Name = Name;
+        this.Damage = Damage;
+    }
+    public void PrintCharacterInfo() 
+    {
+        Console.WriteLine($"{Name} HP { Health}");
+    }
+    
+}
+class NPC : Character
+{
+    public NPC( int HP,string NameOfCharacter, int Damage): base(HP,NameOfCharacter,Damage)
+    {
+    }
+}
+class Enemy : Character
+{
+    public Enemy(int HP, string NameOfCharacter, int Damage) : base(HP, NameOfCharacter, Damage) 
+    {
+    }
+}
+class Creeper : Enemy
+{
+    public Creeper (int HP, string NameOfCharacter, int Damage) : base(HP, NameOfCharacter, Damage)
+    {
+       
+    }
+    public void CreeperDamage (int damage)
+    {
+        Console.WriteLine("наносит урон" + damage);
+    }
+   
+}
